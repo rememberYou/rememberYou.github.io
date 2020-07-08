9bf89acdc4efd01c62a9b3a0d2739767307db34d
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-07-08 08:12:36 +0200

    Add better support for small resolution

--- components/Header.d.ts
+++ components/Header.d.ts
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import { Theme, WithStyles } from '@material-ui/core';
-declare const styles: (theme: Theme) => Record<"root" | "picture" | "card" | "gitHubIcon" | "icons" | "linkedInIcon" | "mailIcon", import("@material-ui/styles").CSSProperties | import("@material-ui/styles").CreateCSSProperties<{}> | ((props: {}) => import("@material-ui/styles").CreateCSSProperties<{}>)>;
+declare const styles: (theme: Theme) => Record<"root" | "content" | "picture" | "card" | "gitHubIcon" | "icons" | "linkedInIcon" | "mailIcon" | "mainTitle" | "secondTitle", import("@material-ui/styles").CSSProperties | import("@material-ui/styles").CreateCSSProperties<{}> | ((props: {}) => import("@material-ui/styles").CreateCSSProperties<{}>)>;
 interface HeaderProps extends WithStyles<typeof styles> {
     classes: any;
     subtitle: JSX.Element;
     title: string;
 }
-declare const _default: React.ComponentType<Pick<HeaderProps, "title" | "subtitle"> & import("@material-ui/core").StyledComponentProps<"root" | "picture" | "card" | "gitHubIcon" | "icons" | "linkedInIcon" | "mailIcon">>;
+declare const _default: React.ComponentType<Pick<HeaderProps, "title" | "subtitle"> & import("@material-ui/core").StyledComponentProps<"root" | "content" | "picture" | "card" | "gitHubIcon" | "icons" | "linkedInIcon" | "mailIcon" | "mainTitle" | "secondTitle">>;
 export default _default;
