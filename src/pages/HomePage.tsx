de528c8941410599766bedcc7eff2090d5b41eb6
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-07-17 16:05:19 +0200

    Refactoring the Home component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,10 +3,8 @@ import * as React from 'react';
 import Header from '../components/Header';
 import { subtitle, title } from '../constants/header';
 
-class Home extends React.Component {
-  render() {
-    return <Header title={title} subtitle={subtitle} />;
-  }
-}
+const Home = () => {
+  return <Header title={title} subtitle={subtitle} />;
+};
 
 export default Home;
