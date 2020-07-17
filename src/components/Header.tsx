d42d9ad28df81000a905a60578a720913943276c
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-07-17 16:20:11 +0200

    Refactoring the Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,16 +7,15 @@ import {
   IconButton,
   Theme,
   Typography,
-  WithStyles,
   createStyles,
-  withStyles,
+  makeStyles,
 } from '@material-ui/core';
 
 import EmailIcon from '@material-ui/icons/Email';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
-const styles = (theme: Theme) =>
+const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     card: {
       display: 'flex',
@@ -26,7 +25,7 @@ const styles = (theme: Theme) =>
       backgroundColor: '#fafafa',
     },
     content: {
-      paddingBottom: '0'
+      paddingBottom: '0',
     },
     gitHubIcon: {
       fontSize: theme.spacing(4),
@@ -45,15 +44,15 @@ const styles = (theme: Theme) =>
       color: '#C64D42',
     },
     mainTitle: {
-      fontSize: "2.125rem",
+      fontSize: '2.125rem',
       [theme.breakpoints.up('sm')]: {
-        fontSize: "3rem",
+        fontSize: '3rem',
       },
     },
     secondTitle: {
-      fontSize: "1.25rem",
+      fontSize: '1.25rem',
       [theme.breakpoints.up('sm')]: {
-        fontSize: "1.5rem",
+        fontSize: '1.5rem',
       },
     },
     picture: {
@@ -85,73 +84,66 @@ const styles = (theme: Theme) =>
         flexDirection: 'row',
       },
     },
-  });
+  }),
+);
 
-interface HeaderProps extends WithStyles<typeof styles> {
-  classes: any;
+interface HeaderProps {
   subtitle: JSX.Element;
   title: string;
 }
 
-class Header extends React.Component<HeaderProps> {
-  render() {
-    const { classes, subtitle, title } = this.props;
-    return (
-      <Fade in={true} timeout={1000}>
-        <div className={classes.root}>
-          <Card className={classes.card} elevation={0}>
-            <CardContent className={classes.content}>
-              <Typography
-                align="center"
-                className={classes.mainTitle}
-                variant="h3"
-                component="h1"
-                gutterBottom
-              >
-                {title}
-              </Typography>
-              <Typography
-                align="center"
-                className={classes.secondaryTitle}
-                variant="h5"
-                color="textSecondary"
-                component="h2"
-              >
-                {subtitle}
-              </Typography>
-            </CardContent>
-            <div className={classes.icons}>
-              <IconButton aria-label="GitHub" target="_blank" href="https://github.com/rememberYou">
-                <GitHubIcon className={classes.gitHubIcon} />
-              </IconButton>
-              <IconButton
-                color="primary"
-                aria-label="LinkedIn"
-                target="_blank"
-                href="https://www.linkedin.com/in/terencioagozzino/"
-              >
-                <LinkedInIcon className={classes.linkedInIcon} />
-              </IconButton>
-              <IconButton
-                color="secondary"
-                aria-label="Mail"
-                target="_blank"
-                href="mailto:terencio.agozzino@gmail.com"
-              >
-                <EmailIcon className={classes.mailIcon} />
-              </IconButton>
-            </div>
-          </Card>
-          <Avatar
-            alt="Terencio Agozzino"
-            variant="square"
-            src={require('../assets/picture.jpg')}
-            className={classes.picture}
-          />
-        </div>
-      </Fade>
-    );
-  }
-}
+const Header = (props: HeaderProps) => {
+  const classes = useStyles();
+  const { subtitle, title } = props;
+  return (
+    <Fade in={true} timeout={1000}>
+      <div className={classes.root}>
+        <Card className={classes.card} elevation={0}>
+          <CardContent className={classes.content}>
+            <Typography
+              align="center"
+              className={classes.mainTitle}
+              variant="h3"
+              component="h1"
+              gutterBottom
+            >
+              {title}
+            </Typography>
+            <Typography align="center" variant="h5" color="textSecondary" component="h2">
+              {subtitle}
+            </Typography>
+          </CardContent>
+          <div className={classes.icons}>
+            <IconButton aria-label="GitHub" target="_blank" href="https://github.com/rememberYou">
+              <GitHubIcon className={classes.gitHubIcon} />
+            </IconButton>
+            <IconButton
+              color="primary"
+              aria-label="LinkedIn"
+              target="_blank"
+              href="https://www.linkedin.com/in/terencioagozzino/"
+            >
+              <LinkedInIcon className={classes.linkedInIcon} />
+            </IconButton>
+            <IconButton
+              color="secondary"
+              aria-label="Mail"
+              target="_blank"
+              href="mailto:terencio.agozzino@gmail.com"
+            >
+              <EmailIcon className={classes.mailIcon} />
+            </IconButton>
+          </div>
+        </Card>
+        <Avatar
+          alt="Terencio Agozzino"
+          variant="square"
+          src={require('../assets/picture.jpg')}
+          className={classes.picture}
+        />
+      </div>
+    </Fade>
+  );
+};
 
-export default withStyles(styles)(Header);
+export default Header;
