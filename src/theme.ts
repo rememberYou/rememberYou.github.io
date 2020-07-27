1129fac86359566010332c99566b0a4dc09b2979
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-07-27 22:26:10 +0200

    refactor: remove default palette colors

--- src/theme.ts
+++ src/theme.ts
@@ -1,19 +1,6 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import { blue, red } from '@material-ui/core/colors';
 
 export default createMuiTheme({
-  palette: {
-    primary: {
-      light: blue[300],
-      main: blue[500],
-      dark: blue[700],
-    },
-    secondary: {
-      light: red[300],
-      main: red[500],
-      dark: red[700],
-    },
-  },
   typography: {
     fontFamily: [
       '-apple-system',
