72fa8c75b003e89207be6212c9c7a892d1e73908
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-07-30 19:45:25 +0200

    Updates

--- constants/social-links.d.ts
+++ constants/social-links.d.ts
@@ -1,6 +1,7 @@
 export declare const SOCIAL_LINKS: {
     color: string;
     icon: import("@material-ui/core/OverridableComponent").OverridableComponent<import("@material-ui/core").SvgIconTypeMap<{}, "svg">>;
+    label: string;
     title: string;
     url: string;
 }[];
