245e6c3d7c1a63b094feb23c2b6c6f3ae2a83765
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-08-03 09:22:09 +0200

    Updates

--- constants/contacts.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const EMAIL = "terencio.agozzino@gmail.com";
-export declare const GITHUB = "https://github.com/rememberYou";
-export declare const LINKEDIN = "https://www.linkedin.com/in/terencioagozzino";
--- constants/links.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const SOCIAL_LINKS: {
-    color: string;
-    icon: import("@material-ui/core/OverridableComponent").OverridableComponent<import("@material-ui/core").SvgIconTypeMap<{}, "svg">>;
-    title: string;
-    url: string;
-}[];
